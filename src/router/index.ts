85c6414ec627bf0c683f4c18e4a1f3018349fe7c
Ganesha Ampuh <ganeshaampuh@gmail.com>
2025-06-23 08:05:52 +0700

    Update router configuration to add an alias for the home route and modify profile redirection to include a query parameter for section. This improves navigation and user experience.

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      alias: '/',
       path: '/home',
       name: 'home',
       component: HomeView,
@@ -58,7 +59,7 @@ router.beforeEach(async (to, from, next) => {
   if (to.meta.requiresAuth && !session) {
     next('/login')
   } else if (!to.meta.requiresAuth && session) {
-    next('/profile')
+    next('/profile?section=basic')
   } else {
     next()
   }
